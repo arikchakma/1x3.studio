cdd2e443a0353b4f11f331f09eee93eb25d7ab05
Arik Chakma <arikchangma@gmail.com>
2022-02-22 04:37:28 +0600

    Changed breakpoints config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,34 @@
 module.exports = {
-	mode: 'jit',
-	content: [
-		'./pages/**/*.{js,ts,jsx,tsx}',
-		'./components/**/*.{js,ts,jsx,tsx}',
-		'./layouts/**/*.{js,ts,jsx,tsx}',
-	],
-	darkMode: 'class',
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  mode: 'jit',
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js,ts,jsx,tsx}',
+    './icons/**/*.{js,ts,jsx,tsx}'
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      // BREAKPOINT SCREEN
+      screens: {
+        '2xl': { max: '1535px' },
+        // => @media (max-width: 1535px) { ... }
+
+        xl: { max: '1279px' },
+        // => @media (max-width: 1279px) { ... }
+
+        lg: { max: '1023px' },
+        // => @media (max-width: 1023px) { ... }
+
+        md: { max: '767px' },
+        // => @media (max-width: 767px) { ... }
+
+        sm: { max: '639px' },
+        // => @media (max-width: 639px) { ... }
+
+        '2sm': { max: '480px' }
+      }
+    }
+  },
+  plugins: []
 };
